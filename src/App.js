db8d315ddb2120279def1de939825608f9563a42
kedm717 <dongchan1583@gmail.com>
2021-08-24 23:31:47 +0900

    최종 완성 및 배포

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import Home from './routers/Home'
 import './App.css';
 import {HashRouter, Route} from 'react-router-dom'
 import About from './routers/About'
+import Navigation  from './components/Navigation';
+import Detail from './routers/Detail';
 
 
 function App(){
   return (
     <HashRouter>
-      <Route path ="/" exact={true} component={Home}/>
-      <Route path ="/about" component={About}/>
+      <Navigation />
+      <Route path ="/" exact={true} component={Home} />
+      <Route path ="/about" component={About} />
+      <Route path ="/movie-detail" component={Detail} />
     </HashRouter>
   );
 }
